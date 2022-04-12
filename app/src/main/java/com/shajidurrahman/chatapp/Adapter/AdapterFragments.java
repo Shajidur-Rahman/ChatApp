702bec787f32b07098bb4c3cc5b3b8983735b453
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-04-12 14:43:21 +0600

    some files added

--- app/src/main/java/com/shajidurrahman/chatapp/Adapter/AdapterFragments.java
+++ app/src/main/java/com/shajidurrahman/chatapp/Adapter/AdapterFragments.java
@@ -1,2 +1,35 @@
-package com.shajidurrahman.chatapp.Adapter;public class AdapterFragments {
+package com.shajidurrahman.chatapp.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.shajidurrahman.chatapp.Fragments.CallsFragment;
+import com.shajidurrahman.chatapp.Fragments.ChatsFragment;
+import com.shajidurrahman.chatapp.Fragments.StatusFragment;
+
+public class AdapterFragments extends FragmentPagerAdapter {
+    public AdapterFragments(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0: return new ChatsFragment();
+            case 1: return new StatusFragment();
+            case 2: return new CallsFragment();
+            default: return new ChatsFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
 }
